d847acbfaa23d79c034384dfc2acfb5e7fbe9739
Victorious <cephvickie@gmail.com>
2022-06-11 15:52:26 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10)
+let num3 = 39
+let num4 = 5
+let mod = num3 % num4
 
-// Write your code here
+let max = 20
